0356ba061ea3663cfe45e67f0902587860145b01
jchen706 <>
2019-11-02 19:50:04 -0400

    added file upload, and form inputs

--- 2ndgen.py
+++ 2ndgen.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template, session, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
+
+import PyPDF2
+import json
+
+from flask import Flask, render_template, session, redirect, url_for, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 
-app = Flask(__name__)
+#change this path to a folder for the upload
+UPLOAD_FOLDER = 'C:/Users/jchen/Documents/2ndGenSports/static/pdfs'
 
+ALLOWED_EXTENSIONS = {'txt'}
+DEBUG = True
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my security sucks'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 class MediaForm(FlaskForm):
 
@@ -15,29 +30,62 @@ class MediaForm(FlaskForm):
     gend = SelectField('Gender: ', choices=[('M','Male'), ('F','Female')], validators = [DataRequired()] )
     submit = SubmitField('Submit')
 
+@app.route('/uploadpdf',methods = ['GET','POST'])
+def newIndex():
+    if(request.method == "POST"):
+
+        if request.files:
+
+            file1 = request.files["pdffile"]
+
+            if (file.filename == ''):
+                flash('No file selected')
+
+            if(file and allowed_file(file.filename)):
+                file1.save(os.path.join(app.config['UPLOAD_FOLDER'], file1.filename))
+                print('file save')
+
+
+            return redirect(request.url)
+
+    return render_template("uploadpdf.html")
+
+
 @app.route('/',methods = ['GET','POST'])
 def index():
     year = False
+    #form = MediaForm()
 
-    form = MediaForm()
+    if(request.method == "POST"):
 
-    if form.validate_on_submit():
+        year = request.form['year']
+        team = request.form['team']
+        gender = request.form['gender']
+        sport = request.form['sport']
 
-        session['year'] = form.year.data
-        session['team'] = form.team.data
-        session['sport'] = form.sport.data
-        session['gend'] = form.gend.data
+        print(year)
+        print(team)
+        print(gender)
+        print(sport)
 
-        form.year.data = ''
-        form.team.data = ''
-        form.sport.data = ''
-        form.gend.data = ''
-        return redirect(url_for('successful'))
-    return render_template('home.html', form = form)
+        if request.files:
+
+            file1 = request.files["pdffile"]
+
+            if (file1.filename == ''):
+                flash('No file selected')
+
+            if(file1 and allowed_file(file1.filename)):
+                file1.save(os.path.join(app.config['UPLOAD_FOLDER'], file1.filename))
+                print('file save')
+
+    else:
+        return render_template('home.html')
+    return render_template('home.html')
 
 @app.route('/successful')
 def successful():
     return render_template('successful.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
