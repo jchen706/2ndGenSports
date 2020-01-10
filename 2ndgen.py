2a1a3d0ee70984188d83e2f58ce2c024107483fe
jgonzalez314 <50882675+jgonzalez314@users.noreply.github.com>
2020-01-10 12:09:40 -0500

    Add files via upload

--- 2ndgen.py
+++ 2ndgen.py
@@ -1,21 +1,76 @@
-from flask import Flask, render_template, session, redirect, url_for, request
+from flask import Flask, render_template, session, redirect, url_for, request, abort, flash
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired
-from werkzeug.utils import secure_filename
 import os
+from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER ='./uploads'
+import nltk
+
+nltk.download('punkt')
+from nltk.corpus import PlaintextCorpusReader
+from nltk.tokenize import TweetTokenizer, sent_tokenize
+
+import PyPDF2
+import json
+#import textract
+
+import tika
+tika.initVM()
+from tika import parser
+#import mysql.connector
+#from mysql.connector import Error
+
+import re
+
+from nltk.tokenize import sent_tokenize
+
+
+
+
+
+
+#from flask_sqlalchemy import SQLAlchemy
+#import SQLAlchemy
+
+#absolute path of the file
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+#change this path to a folder for the upload
+UPLOAD_FOLDER = '/static/pdfs'
+
 ALLOWED_EXTENSIONS = {'pdf'}
-
+DEBUG = True
 app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1547@localhost/alchemy'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['SECRET_KEY'] = '1111'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+#db = SQLAlchemy(app)
 
-app.config['SECRET_KEY'] = 'my security sucks'
+# class PdfFile(db.Model):
+#     """docstring for ."""
+#     id = db.Column(db.Integer, primary_key = True)
+#     year = db.Column(db.Integer,)
+#
+#     def __init__(self, arg):
+#         super(, self).__init__()
+#         self.arg = arg
+
+
+@app.errorhandler(400)
+def bad_request(e):
+     return render_template('400.html'), 400
+
+
+
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 class MediaForm(FlaskForm):
 
@@ -23,37 +78,326 @@ class MediaForm(FlaskForm):
     team = StringField('Team:', validators = [DataRequired()])
     sport =SelectField('Sport:', choices=[('foot', 'Football'),('basket', 'BasketBall')], validators = [DataRequired()] )
     gend = SelectField('Gender: ', choices=[('M','Male'), ('F','Female')], validators = [DataRequired()] )
-    mediapdf = FileField('PDF:', validators=[FileRequired()])
     submit = SubmitField('Submit')
+
+@app.route('/uploadpdf',methods = ['GET','POST'])
+def newIndex():
+    if(request.method == "POST"):
+
+        if request.files:
+
+            file1 = request.files["pdffile"]
+
+            if (file.filename == ''):
+                flash('No file selected')
+
+            if(file and allowed_file(file.filename)):
+                file1.save(os.path.join(app.config['UPLOAD_FOLDER'], file1.filename))
+                print('file save')
+
+
+            return redirect(request.url)
+
+    return render_template("uploadpdf.html")
+
 
 @app.route('/',methods = ['GET','POST'])
 def index():
-    year = False
+    processed = False
+    year = 0000
+    team = "No Team"
+    gender = "No Input"
+    sport = "No Input"
+    list1 = []
 
-    form = MediaForm()
+    error_year = False
+    error_team = False
+    error_gender = False
+    error_sport = False
+    error_file = False
 
-    if form.validate_on_submit():
+    #form = MediaForm()
+    print(BASE_DIR)
 
-        session['year'] = form.year.data
-        session['team'] = form.team.data
-        session['sport'] = form.sport.data
-        session['gend'] = form.gend.data
-        session['mediapdf']= form.mediapdf.data
+    file_path = BASE_DIR + app.config['UPLOAD_FOLDER']
+    print(file_path)
+
+    #database server connection ...
+    # try:
+    #     connection = mysql.connector.connect(host='localhost',
+    #                                      database='sportgen',
+    #                                      user='root',
+    #                                      password='1547')
+
+    #     if connection.is_connected():
+    #         db_Info = connection.get_server_info()
+    #         print("Connected to MySQL Server version ", db_Info)
+    #         cursor = connection.cursor()
+    #         cursor.execute("select database();")
+    #         record = cursor.fetchone()
+    #         print("You're connected to database: ", record)
+    # except Error as e:
+    #     print("Error while connecting to MySQL", e)
+    # finally:
+    #     if (connection.is_connected()):
+    #         cursor.close()
+    #         connection.close()
+    #         print("MySQL connection is closed")
 
 
-        form.year.data = ''
-        form.team.data = ''
-        form.sport.data = ''
-        form.gend.data = ''
-        form.mediapdf.file = ''
-        return redirect(url_for('successful'))
-    return render_template('home.html', form = form)
+    #action posted from frontend
+    if(request.method == "POST"):
 
-@app.route('/successful')
+        year = request.form['year']
+        team = request.form['team']
+        gender = request.form['gender']
+        sport = request.form['sport']
+        processed = True
+
+
+
+
+
+        try:
+            year = int(year)
+        except:
+            error_year = True
+
+        try:
+            year = int(year)
+        except:
+            error_year = True
+
+
+
+
+
+
+
+
+
+
+
+        #count
+        #pdf
+
+        print(year)
+        print(team)
+        print(gender)
+        print(sport)
+
+        if request.files:
+
+            file1 = request.files["pdffile"]
+            print(file1.filename == '')
+
+            if (file1.filename == ''):
+                flash('No file selected')
+                abort(400, description="No file submitted.")
+
+            print(allowed_file(file1.filename))
+            if(allowed_file(file1.filename)):
+                file1.save(os.path.join(file_path, file1.filename))
+                file = open(os.path.join(file_path, file1.filename), 'r')
+
+
+
+
+
+                # read_file = file.read()
+                # text = nltk.Text(nltk.word_tokenize(read_file))
+
+                # match = text.concordance('parent')
+                # print(match)
+                # creating a pdf file object
+                #
+                pdfFileObj = open(os.path.join(file_path, file1.filename), 'rb')
+
+                # creating a pdf reader object
+                pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+                # printing number of pages in pdf file
+                print(pdfReader.numPages)
+
+                # creating a page object
+                pageObj = pdfReader.getPage(0)
+
+                # extracting text from page
+                print(pageObj.extractText())
+                #text = textract.process(os.path.join(app.config['UPLOAD_FOLDER'], file1.filename), extension='pdf')
+
+                parsed = parser.from_file(os.path.join(file_path, file1.filename))
+                #print(parsed["metadata"])
+                #print(parsed["content"])
+                input_text = parsed['content']
+
+                tokenizer_words = TweetTokenizer()
+                tokens_sentences = [tokenizer_words.tokenize(t) for t in nltk.sent_tokenize(input_text)]
+                #print(tokens_sentences)
+                wantedList=['parent','Parents', 'Father', 'Mother', 'father', 'mother', 'dad', 'Dad', 'Mom', 'mom']
+                count = 0
+
+                diction1 = {}
+                count1 = 0
+                list10 = []
+                for eachSentence in tokens_sentences:
+                    diction1[count1] = []
+                    for each in wantedList:
+                        if each in eachSentence:
+                            #print(eachSentence)
+                            #print(" ")
+                            #print(" ")
+                            aSentence =' '.join(eachSentence)
+                            diction1[count1].append(aSentence)
+                            count1+=1
+                            list10.append(aSentence)
+                            break
+                dictiona = {}
+                for j in range(len(list10)):
+                     dictiona[j] = []
+                     list00 = list10[j].split(' ')
+                     for i in range(len(list00)):
+                         for eachword in wantedList:
+                                if (eachword == list00[i]):
+                                   list00[i] = eachword
+                                   if (len(list00) < 20):
+                                       dictiona[j].append(list00)
+                                       break
+                                   else:
+                                     try:
+                                         #print(list33[i-15:i+20])
+                                         a = list00[i-15:i+20]
+                                         dictiona[j].append(a)
+                                     except:
+                                        #print('pass')
+                                        a = list00[i:]
+                                        dictiona[j].append(a)
+                                        #dic[count].append(a)
+
+                print('hereeeee')
+
+
+                list33 = []
+                print(len(dictiona))
+                for key in dictiona:
+                    for ij in range(len(dictiona[key])):
+                         if(len(dictiona[key][0]) > 0):
+                             aSentence =' '.join(dictiona[key][0])
+                             list33.append(aSentence)
+                             break
+
+
+
+
+
+
+                dic = {}
+                list3 = []
+                for key in diction1:
+                    count = 0
+                    if(len(diction1[key]) >=1):
+                        string = diction1[key][0]
+                        list2 = diction1[key][0].split(' ')
+                        dic[count] = []
+                        for i in range(len(list2)):
+                            for eachword in wantedList:
+                                if (eachword == list2[i]):
+                                   if (len(list2) < 20):
+                                       dic[count].append(list2)
+                                       break
+                                       list3.append(a)
+                                   else:
+                                     try:
+                                         print(list2[i-15:i+20])
+                                         a = list2[i-15:i+20]
+                                         dic[count].append(a)
+                                         list3.append(a)
+                                     except:
+                                        print('pass')
+                                        a = list2[i:]
+                                        list3.append(a)
+                                        dic[count].append(a)
+                    count+=1
+
+
+                print(' ')
+                print(' ')
+                print(' here')
+                print(dic)
+                print(len(dic))
+
+                print(list3)
+
+
+
+                        # list1
+                        # for eachword in wantedList:
+                        #    try:
+                        #        index = string.index(eachword)
+
+
+
+                        #    except:
+
+                        # r = re.compile(r'\b%s\b' % word, re.I)
+                        # m = r.search(string)
+                        # index = m.start()
+                    #    a = re.search(r'\b(father)\b', diction1[key][0])
+                #        count12 = 0
+                #        for eachsegment in b:
+                #           container = []
+                #           for eachword in wantedList:
+                #             if eachword in eachsegment:
+                #                 container.append(eachsegment)
+                #        dic[count12] = container
+                #        count12+=1
+
+                # print(dic)
+
+
+
+
+                #print(count)
+                # for i in range(count):
+                #     print(list[i])
+                #     print('')
+
+                # list3 = []
+                # for i in range(len(list1)):
+                #     b = list1[i].split('•')
+                #     list3.append(b)
+
+                # diction = {}
+                # for j in range(len(list3)):
+                #     diction[j] = []
+                #     count = 0
+                #     for eachSen in list3[j]:
+                #         for each in wantedList:
+                #             if each in eachSen:
+                #                 diction[j].append(eachSen)
+
+
+                # closing the pdf file object
+                pdfFileObj.close()
+                print('file save')
+
+                return render_template('home.html', processed = processed, team_name =team , team_year=year, team_gender=gender, team_sport=sport, list1 = list33, len1 = len(list33))
+        else:
+            abort(400, description="No file submitted.")
+
+
+
+    else:
+
+
+        return render_template('home.html', processed = processed, team_name =team , team_year=year, team_gender=gender, team_sport=sport, list1 = list1, len1 = len(list1))
+
+
+    return render_template('home.html',processed = processed, team_name =team , team_year=year, team_gender=gender, team_sport=sport, list1 = list1,len1 = len(list1))
+
+@app.route('/processing')
 def successful():
-    return render_template('successful.html')
-
-
+    return render_template('process.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
