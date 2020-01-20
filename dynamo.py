054c4cb354c6dcab60a90254fdfb382f9393ce28
pchuh <phillip.hong1483@gmail.com>
2020-01-19 19:59:38 -0500

    implemented putItem method

--- dynamo.py
+++ dynamo.py
@@ -1,71 +1,71 @@
 import boto3 
 
-__TableName__ = "2ndGen" 
+#name of the dynamoDB table
+__TableName__ = "2ndGenSports" 
 
 client = boto3.client('dynamodb') 
 
 DB = boto3.resource('dynamodb') 
 
+#construct the appropriate table object based on __TableName__
 table = DB.Table(__TableName__)  
 
-aString = "hello"
+DEBUG = False 
 
-def getItem():
+# method used to add an item into the database
+def putItem(sport, teamName, year, count): 
 
-	response = table.get_item(
-		Key = {
-			"UserId": "Bob"
-		} 
-	) 
+	#checking if sport variable is a string
+	if not (type(sport) == str):
+		sport = str(sport) 
 	
-	print(response["Item"]) 
-
-def putItem():
+	#checking if teamName variable is a string
+	if not (type(teamName) == str):
+		teamName = str(teamName)
 	
+	#checking if year variable is an int
+	if not (type(year) == int):
+		year = int(year)
+	
+	#checking if count variable is an int
+	if not (type(count) == int):
+		count = int(count) 
+
+	
+	#checking if sport and teamName are all lowercase and them of beginning and ending whitespace
+	if not (sport.islower()):
+		sport = sport.lower()  
+
+	sport = sport.strip()
+
+	if not (teamName.islower()):
+		teamName = teamName.lower()
+
+	teamName = teamName.strip()
+	
+
+	#creating ID which is used to uniquely identify the data in the database
+	ID = sport + teamName + str(year)
+	
+	#calling put_item method on the table which takes a dictionary as its parameter. The dictionary maps table column name(str) to value. 
 	response = table.put_item(  
 		Item = {
-			"UserId":"Phillip"
+			"ID": ID
+			,  
+			"Sport": sport 
+			,
+			"TeamName": teamName 
 			, 
-			"Age": '20'
+			"Year": year
 			, 
-			"Height": '44'
-			, 
-			"Income": '20'
-		}
-	)  
-	print(response) 
-
-def deleteItem():
-	response = table.delete_item(
-		Key = {
-			"UserId" : "Phillip"
+			"Count": count
 		} 
-	) 
+	)   
 
-def updateItem():
-	response = table.update_item(
-		Key = {
-			"UserId" : "Phillip"
-		}, 
-		UpdateExpression = "set Income = :i", 
-		ExpressionAttributeValues = {
-			':i' : 2000
-		} 
+	if (DEBUG): 
+		print(response)  
 
 
-		# AttributeUpdates = {
-		# 	"Income": '2000'
-		# } 
-	)
-def printHi():
-	print(aString) 
 
-
- 
-if __name__ == '__main__': 
-	# getItem() 
-	# putItem() 
-	# deleteItem()
-	# updateItem() 
-
-	print('this is the main method')
+if __name__ == '__main__':
+	putItem("basketball", "duke", 2018, 8)
