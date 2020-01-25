5367318c1debde964c12c848689f7c49309fd365
justinmikechiu <justinmikechiu@gmail.com>
2020-01-25 16:54:36 -0500

    fixed dynamo.py in justin branch

--- dynamo.py
+++ dynamo.py
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-import boto3 
-
-#name of the dynamoDB table
-__TableName__ = "2ndGenSports" 
-
-client = boto3.client('dynamodb') 
-
-DB = boto3.resource('dynamodb') 
-
-#construct the appropriate table object based on __TableName__
-table = DB.Table(__TableName__)  
-
-DEBUG = False 
-
-# method used to add an item into the database
-def putItem(sport, teamName, year, count): 
-
-	#checking if sport variable is a string
-	if not (type(sport) == str):
-		sport = str(sport) 
-	
-	#checking if teamName variable is a string
-	if not (type(teamName) == str):
-		teamName = str(teamName)
-	
-	#checking if year variable is an int
-	if not (type(year) == int):
-		year = int(year)
-	
-	#checking if count variable is an int
-	if not (type(count) == int):
-		count = int(count) 
-
-	
-	#checking if sport and teamName are all lowercase and them of beginning and ending whitespace
-	if not (sport.islower()):
-		sport = sport.lower()  
-=======
 import boto3
 
 #name of the dynamoDB table
@@ -74,7 +35,6 @@ def putItem(sport, teamName, year, count):
 	#checking if sport and teamName are all lowercase and them of beginning and ending whitespace
 	if not (sport.islower()):
 		sport = sport.lower()
->>>>>>> e6e80fb64f3a13a33de093bb8eda3ec3170fa0ca
 
 	sport = sport.strip()
 
@@ -82,49 +42,6 @@ def putItem(sport, teamName, year, count):
 		teamName = teamName.lower()
 
 	teamName = teamName.strip()
-<<<<<<< HEAD
-	
-
-	#creating ID which is used to uniquely identify the data in the database
-	ID = sport + teamName + str(year)
-	
-	#calling put_item method on the table which takes a dictionary as its parameter. The dictionary maps table column name(str) to value. 
-	response = table.put_item(  
-		Item = {
-			"ID": ID
-			,  
-			"Sport": sport 
-			,
-			"TeamName": teamName 
-			, 
-			"Year": year
-			, 
-			"Count": count
-		} 
-	)   
-
-	if (DEBUG): 
-		print(response)   
-
-def getItem(sport, teamName, year): 
-
-	#checking if sport variable is a string
-	if not (type(sport) == str):
-		sport = str(sport) 
-	
-	#checking if teamName variable is a string
-	if not (type(teamName) == str):
-		teamName = str(teamName)
-	
-	#checking if year variable is an int
-	if not (type(year) == int):
-		year = int(year)
-	
-	
-	#checking if sport and teamName are all lowercase and them of beginning and ending whitespace
-	if not (sport.islower()):
-		sport = sport.lower()  
-=======
 
 
 	#creating ID which is used to uniquely identify the data in the database
@@ -166,7 +83,6 @@ def getItem(sport, teamName, year):
 	#checking if sport and teamName are all lowercase and them of beginning and ending whitespace
 	if not (sport.islower()):
 		sport = sport.lower()
->>>>>>> e6e80fb64f3a13a33de093bb8eda3ec3170fa0ca
 
 	sport = sport.strip()
 
@@ -174,11 +90,7 @@ def getItem(sport, teamName, year):
 		teamName = teamName.lower()
 
 	teamName = teamName.strip()
-<<<<<<< HEAD
-	
-=======
 
->>>>>>> e6e80fb64f3a13a33de093bb8eda3ec3170fa0ca
 
 	#creating ID which is used to uniquely identify the data in the database
 	ID = sport + teamName + str(year)
@@ -186,28 +98,13 @@ def getItem(sport, teamName, year):
 	response = table.get_item(
 		Key = {
 			"ID": ID
-<<<<<<< HEAD
-		} 
-	) 
-	
-	return response["Item"] 
-=======
 		}
 	)
 
 	return response["Item"]
->>>>>>> e6e80fb64f3a13a33de093bb8eda3ec3170fa0ca
 
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-	putItem("basketball", "duke", 2018, 8) 
-	print(getItem("basketball", "duke", 2018))
-
-	
-
-=======
 	putItem("basketball", "duke", 2018, 8)
 	print(getItem("basketball", "duke", 2018))
->>>>>>> e6e80fb64f3a13a33de093bb8eda3ec3170fa0ca
