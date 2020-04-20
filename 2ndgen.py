07413813ea657344c45aba71a6516f767c1eda52
pchuh <phillip.hong1483@gmail.com>
2020-04-19 20:51:36 -0400

    updated scraper to be compatible with jobs

--- 2ndgen.py
+++ 2ndgen.py
@@ -550,64 +550,9 @@ def processScraper():
         return_dict = None
         error_scraper = False
 
-        try:
-            job = q.enqueue(base_scraper, roster_url, base_url)  
-            # return_dict = base_scraper(roster_url, base_url)
-        except:
-            return render_template('404.html')
+        job = q.enqueue(workerProcessScraper, year, gender, sport, team, roster_url, base_url)   
 
-
-        while(job.result == None): 
-            time.sleep(.1)
-
-        return_dict = job.result 
-
-        if return_dict == None:
-
-            return render_template('scraperx.html', teamId="nothing is processed", processed=processed)
-
-
-
-
-
-        if error_scraper:
-
-            return render_template('scraperx.html', teamId="nothing is processed", processed=processed)
-
-        else:
-
-            true_dict = return_dict.copy()
-            for key1, value in true_dict.items():
-                if len(value) == 0:
-                    return_dict.pop(key1, None)
-
-
-
-
-            keyWordCountDict = {}
-            for key, value in return_dict.items():
-
-                for each in keyWordList:
-
-                    for bullet in value:
-
-                        if each in bullet.lower().split():
-
-                            if (each in keyWordCountDict):
-                                keyWordCountDict[each] += 1
-                                print("found " + each + " for " + key)
-                                break
-                            else:
-                                keyWordCountDict[each] = 1
-                                print("found " + each + " for " + key)
-                                break
-
-
-
-
-            return render_template('scraperx.html', returnTeam=return_dict, processed=processed,
-            team_name =team , team_year=year, team_gender=gender, team_sport=sport, keyWordList = keyWordList,
-            keyWordCountKeys = keyWordCountDict.keys(), keyWordCountDict = keyWordCountDict, length_dict = len(return_dict))
+        return ('', 204)
 
 
 
@@ -622,7 +567,62 @@ def processScraper():
 
 
 
-    return render_template('scraperx.html', teamId="nothing is processed", processed=processed)
+    return render_template('scraperx.html', teamId="nothing is processed", processed=processed) 
+
+def workerProcessScraper(year, gender, sport, team, roster_url, base_url):
+
+    try:
+        return_dict = base_scraper(roster_url, base_url)
+    except:
+        return render_template('404.html')
+
+
+    if return_dict == None:
+
+        return render_template('scraperx.html', teamId="nothing is processed", processed=processed)
+
+
+
+
+
+    if error_scraper:
+
+        return render_template('scraperx.html', teamId="nothing is processed", processed=processed)
+
+    else:
+
+        true_dict = return_dict.copy()
+        for key1, value in true_dict.items():
+            if len(value) == 0:
+                return_dict.pop(key1, None)
+
+
+
+
+        keyWordCountDict = {}
+        for key, value in return_dict.items():
+
+            for each in keyWordList:
+
+                for bullet in value:
+
+                    if each in bullet.lower().split():
+
+                        if (each in keyWordCountDict):
+                            keyWordCountDict[each] += 1
+                            print("found " + each + " for " + key)
+                            break
+                        else:
+                            keyWordCountDict[each] = 1
+                            print("found " + each + " for " + key)
+                            break
+
+
+
+
+        return render_template('scraperx.html', returnTeam=return_dict, processed=processed,
+            team_name =team , team_year=year, team_gender=gender, team_sport=sport, keyWordList = keyWordList,
+            keyWordCountKeys = keyWordCountDict.keys(), keyWordCountDict = keyWordCountDict, length_dict = len(return_dict))
 
 
 
